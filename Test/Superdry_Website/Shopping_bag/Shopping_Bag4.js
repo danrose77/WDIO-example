a071f3f3ef537f06d17f726b265e3a81c2221018
Daniel.Rose <daniel.rose@superdry.com>
2019-07-18 11:00:44 +0100

    .

--- Test/Superdry_Website/Shopping_bag/Shopping_Bag4.js
+++ Test/Superdry_Website/Shopping_bag/Shopping_Bag4.js
@@ -10,7 +10,7 @@ describe(specname + ' - 20+ products can be added to the shopping bag', () => {
     it('Open the environment', () => {
         Environment.openBaseURL();
     });
-    it('Go to a random section and return a product and add (' + Qty6 + 1 + ') product to the bag', () => {
+    it('Go to a random section and return a product and add (' + (Qty6 + 1) + ') product to the bag', () => {
         Navigation.randomSection();
         Search.PickRandomProduct();
         Product.SelectASizeAndAddTo('Bag');
