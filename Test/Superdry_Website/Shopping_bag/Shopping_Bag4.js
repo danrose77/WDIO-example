695a630997a31faa8993f3a7abf323a918369e18
Daniel.Rose <daniel.rose@superdry.com>
2019-07-18 10:23:32 +0100

    .

--- Test/Superdry_Website/Shopping_bag/Shopping_Bag4.js
+++ Test/Superdry_Website/Shopping_bag/Shopping_Bag4.js
@@ -6,18 +6,18 @@ import ShoppingBag from "../../../Pages/ShoppingBag";
 
 let Qty6 = 20;
 
-describe(specname+' - 20+ products can be added to the shopping bag', () => {
+describe(specname + ' - 20+ products can be added to the shopping bag', () => {
     it('Open the environment', () => {
         Environment.openBaseURL();
     });
-    it('Go to a random section and return a product and add ('+Qty6+') product to the bag', () => {
+    it('Go to a random section and return a product and add (' + Qty6 + 1 + ') product to the bag', () => {
         Navigation.randomSection();
         Search.PickRandomProduct();
         Product.SelectASizeAndAddTo('Bag');
         ShoppingBag.increaseShoppingBagItems(Qty6);
     });
-    it('Expect number of items in bag to be correct ('+(Qty6 + 1)+')', () => {
-        ShoppingBag.expectShoppingBagItemNumberToBe(Qty6+ 1);
+    it('Expect number of items in bag to be correct (' + (Qty6 + 1) + ')', () => {
+        ShoppingBag.expectShoppingBagItemNumberToBe(Qty6 + 1);
     });
 
 });
