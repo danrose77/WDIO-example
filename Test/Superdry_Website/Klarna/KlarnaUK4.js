18f802e86570e2b4756bf4a9438086207f7a8dc9
Daniel.Rose <daniel.rose@superdry.com>
2019-07-31 10:12:28 +0100

    .

---
+++ Test/Superdry_Website/Klarna/KlarnaUK4.js
@@ -0,0 +1,30 @@
+import Environment from '../../../Pages/Environment.js';
+import Product from '../../../Pages/Product.js';
+import Search from '../../../Pages/Search.js';
+import Navigation from '../../../Pages/Navigation.js';
+import Checkout from "../../../Pages/Checkout";
+import Customer from "../../../Pages/Customer";
+import AdminPortal from "../../../Pages/AdminPortal";
+
+let username = 'danrosetest+klarnaUK_user@gmail.com';
+
+describe(specname+' - Klarna UK pay later payment from an existing user', () => {
+    it('Set up a customer account', () => {
+        Environment.openBaseURL();
+        Customer.setUpNewAccount(username);
+        Customer.addDeliveryAddress();
+    });
+    it('Go to website and log in', () => {
+        Environment.openBaseURL();
+        Customer.signIn(username);
+    });
+    it('Go to a random section and add a product to the shopping bag', () => {
+        Navigation.randomSection();
+        Search.PickRandomProduct();
+        Product.SelectASizeAndAddTo('Bag');
+    }, 3);
+    it('Go to the checkout as a guest and pay by Klarna', () => {
+        Navigation.GoToCheckout();
+        Checkout.payByKlarna();
+    });
+});
