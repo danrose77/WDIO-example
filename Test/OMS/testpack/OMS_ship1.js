f129ba7fb8df8e7805c14ddcf57d8d552ef9e5de
Daniel.Rose <daniel.rose@superdry.com>
2019-09-09 08:26:23 +0100

    .

--- Test/OMS/testpack/OMS_ship1.js
+++ Test/OMS/testpack/OMS_ship1.js
@@ -4,16 +4,31 @@ import Navigation from '../../../Pages/B2C/Navigation.js';
 import Checkout from "../../../Pages/B2C/Checkout";
 import Rundeck from "../../../Pages/Rundeck";
 import OMS from "../../../Pages/OMS";
+import AdminPortal from "../../../Pages/AdminPortal";
 
 let SKU1 = '1020200500313OI6003';
 let Qty1 = 1;
 
-describe(specname+' - Line: Single - Quantity: Single - Payment: Card - Created -> Scheduled -> Released -> Shipped', () => {
+describe(specname + ' - setup test', () => {
+    it('Set up in admin portal', () => {
+        Environment.openBaseURL();
+        AdminPortal.login();
+        AdminPortal.disableCaptcha();
+        AdminPortal.ensureStockInFrontEnd(SKU1);
+        AdminPortal.colOrderPrefix(true);
+        Environment.openURL("https://sup-oms.qa.coc.ibmcloud.com/smcfs/yfshttpapi/yantrahttpapitester.jsp");
+        OMS.inventoryAdjuster(SKU1, 0, '080');
+        OMS.inventoryAdjuster(SKU1, 0, '090');
+        OMS.inventoryAdjuster(SKU1, 1000, '110');
+    });
+});
+
+describe(specname + ' - Line: Single - Quantity: Single - Payment: Card - Created -> Scheduled -> Released -> Shipped', () => {
     it('Open the environment', () => {
         Environment.openBaseURL();
     });
-    it('Go to SKU: '+SKU1+' and add ('+Qty1+') product to the shopping bag', () => {
-        Environment.goToBasePlus('products/?sku='+SKU1);
+    it('Go to SKU: ' + SKU1 + ' and add (' + Qty1 + ') product to the shopping bag', () => {
+        Environment.goToBasePlus('products/?sku=' + SKU1);
         Product.SelectASizeAndAddTo('Bag', Qty1, true);
         Product.logUsedSKU(SKU1);
     });
@@ -29,7 +44,7 @@ describe(specname+' - Line: Single - Quantity: Single - Payment: Card - Created 
     it('Go to OMS and retrieve order', () => {
         OMS.logIn();
         OMS.retrieveOrder();
-    },);
+    }, );
     it('Then schedule order', () => {
         OMS.scheduleOrder();
     }, 9);
@@ -47,5 +62,16 @@ describe(specname+' - Line: Single - Quantity: Single - Payment: Card - Created 
         OMS.logIn();
         OMS.retrieveOrder();
         OMS.checkForStatus('Shipped')
-    },);
+    }, );
+    it('OMS logout', () => {
+        OMS.logOut();
+    }, );
+});
+
+describe(specname + ' - post run for environment', () => {
+    it('Change colour prefix back', () => {
+        Environment.openBaseURL();
+        AdminPortal.login();
+        AdminPortal.colOrderPrefix(false);
+    });
 });
