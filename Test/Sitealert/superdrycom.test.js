6a7c95276e73481283171b66df35becedd647d69
Daniel.Rose <daniel.rose@superdry.com>
2019-09-10 13:49:54 +0100

    .

--- Test/Sitealert/superdrycom.test.js
+++ Test/Sitealert/superdrycom.test.js
@@ -1,7 +1,7 @@
 import Environment from '../../Pages/B2C/Environment.js';
 
 const sitename = 'https://www.superdry.com/';
-const sitetitle = 'Superdry - Jackets, T Shirts, Hoodies, Shorts, Mens & Womens Clothing - Superdry';
+const sitetitle = 'Superfly - Jackets, T Shirts, Hoodies, Shorts, Mens & Womens Clothing - Superdry';
 
 
 describe(specname+' - Staff user checking that relevant discount is applied', () => {
